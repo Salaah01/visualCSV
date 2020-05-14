8b732b50916f8daefcd5bc6a0f2e8ae8bcc824fb
Salaah-linux-vm <>
2020-05-14 18:11:02 +0100

    docker compose loads correctly

--- client_db_init.sh
+++ client_db_init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" << EOSQL
-
-CREATE CREATE TABLE user_auth (
+psql -v ON_ERROR_STOP=1 --username "$(cat /run/secrets/client_db_user)" --dbname "$(cat /run/secrets/client_db_user)" << EOSQL
+\c postgres
+CREATE TABLE user_auth (
     id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL,
     table_name VARCHAR(100) NOT NULL,
