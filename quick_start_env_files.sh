e7dcb5d42d29ef52e58fb6a1ffc5021cce5e76f8
Salaah-linux-vm <>
2020-05-15 23:36:26 +0100

    quick start env files shell script creates directories if they do not exist

--- quick_start_env_files.sh
+++ quick_start_env_files.sh
@@ -4,22 +4,28 @@
 # the services do work correclty ALL of these envionrment variable should be
 #reaplaced. All environment variables with end with ".env".
 
+echo "Writing .env files..."
+
 # Main client database
+mkdir -p client_db/env/client_db
 echo postgres > client_db/env/client_db/.postgres_db.env
 echo postgres > client_db/env/client_db/.postgres_password.env
 echo postgres > client_db/env/client_db/.postgres_user.env
 
 # Main client test database
+mkdir -p client_db/env/client_test_db
 echo postgres > client_db/env/client_test_db/.postgres_db.env
 echo postgres > client_db/env/client_test_db/.postgres_password.env
 echo postgres > client_db/env/client_test_db/.postgres_user.env
 
 # Django dataase
+mkdir -p django_db/env
 echo postgres > django_db/env/.postgres_db.env
 echo postgres > django_db/env/.postgres_password.env
 echo postgres > django_db/env/.postgres_user.env
 
 # Django application
+mkdir -p web/
 echo DB_ENGINE=django.db.backends.postgresql > web/.web.env
 echo \nDB_HOST=django_db >> web/.web.env
 echo \nDB_USER=postgres >> web/.web.env
@@ -38,3 +44,5 @@ echo \nDB_CLIENT_TEST_USER=postgres >> web/.web.env
 echo \nDB_CLIENT_TEST_NAME=postgres >> web/.web.env
 echo \nDB_CLIENT_TEST_PASSWORD=postgres >> web/.web.env
 echo \nDB_CLIENT_TEST_PORT=5432 >> web/.web.env
+
+echo "Done! Remember to update all .env with new values!"
