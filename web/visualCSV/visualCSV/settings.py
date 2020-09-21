85ccaffa65974cb10c06a69ea05a610ab6a8b13d
Salaah Amin <salaah_amin@hotmail.co.uk>
2020-09-21 03:29:18 +0100

    Update settings.py

--- web/visualCSV/visualCSV/settings.py
+++ web/visualCSV/visualCSV/settings.py
@@ -36,7 +36,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS =['206.189.18.239']
 
 # SECURITY WARNING: keep the secret key used in production secret!
